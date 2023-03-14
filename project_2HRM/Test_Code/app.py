3f50b58a3b5d71e251a79b4513152f1999dff8ad
Abdulsalam26 <117555399+Abdulsalam26@users.noreply.github.com>
2023-03-14 17:31:27 +0530

    Update app.py

--- project_2HRM/Test_Code/app.py
+++ project_2HRM/Test_Code/app.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 from Test_Locators import locators
 from Test_Data import data
